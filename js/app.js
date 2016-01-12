61b1260e7570d66f542098c80fcc493f7f8649c6
phytrisha <markf.meyer@icloud.com>
2016-01-12 20:53:08 +0100

    -sidebar selector

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
 function activateContent (active, deactive) {
 	$("." + active + "Content").addClass("active");
 	$("." + deactive + "Content").removeClass("active");
+	$("#" + active + "Sidebar").addClass("active");
+	$("#" + deactive + "Sidebar").removeClass("active");
+}
+
+function collapseSidebar () {
+	activateCell();
+	$(".contentView").removeClass("active");
 }
 
 function activateCell (cell) {
 	$(".programCell").removeClass("active");
-	$(cell).addClass("active");
-	var previous = $(cell).prev().attr("id");
 	$(".programCell > .programTitle").css("border-bottom-style", "solid");
-	$("#" + cell.id + " > .programTitle").css("border-bottom-style", "none");
-	$("#" + previous + " > .programTitle").css("border-bottom-style", "none");
+	if (cell != undefined) {
+		$(cell).addClass("active");
+		var previous = $(cell).prev().attr("id");
+		$("#" + cell.id + " > .programTitle").css("border-bottom-style", "none");
+		$("#" + previous + " > .programTitle").css("border-bottom-style", "none");
+	}
 }
 
 window.onload = function() {
 	activateContent("programs", "services");
-	$("#sidebarServices").click(function(){
+	$("#servicesSidebar").click(function(){
 		activateContent("services", "programs");
 	});
-	$("#sidebarPrograms").click(function(){
+	$("#programsSidebar").click(function(){
 		activateContent("programs", "services");
 	});
 }
